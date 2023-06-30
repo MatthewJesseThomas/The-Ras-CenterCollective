306cc1b44fe3a3b7b9f6ead5380902c5c411dff1
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-06-30 16:32:21 +0200

    Fin

--- JS/main.js
+++ JS/main.js
@@ -39,3 +39,51 @@ document.addEventListener('DOMContentLoaded', () => {
             });
     });
 });
+// =====Review Code=======================================================
+// Include the star-rating and autosize libraries
+
+// star-rating.js
+// Example CDN link: <script src="https://cdn.jsdelivr.net/npm/star-rating.js/dist/star-rating.min.js"></script>
+
+// autosize.js
+// Example CDN link: <script src="https://cdn.jsdelivr.net/npm/autosize/dist/autosize.min.js"></script>
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Initialize star rating
+    const starrrOptions = {
+        rating: undefined,
+        numStars: 5,
+        change: function (e, value) { }
+    };
+
+    $(".starrr").starrr(starrrOptions);
+
+    // Initialize autosize
+    autosize($('#new-review'));
+
+    let reviewBox = document.getElementById('post-review-box');
+    let newReview = document.getElementById('new-review');
+    let openReviewBtn = document.getElementById('open-review-box');
+    let closeReviewBtn = document.getElementById('close-review-box');
+    let ratingsField = document.getElementById('ratings-hidden');
+
+    openReviewBtn.addEventListener('click', function (e) {
+        reviewBox.style.display = 'block';
+        autosize.update(newReview);
+        newReview.focus();
+        openReviewBtn.style.display = 'none';
+        closeReviewBtn.style.display = 'block';
+    });
+
+    closeReviewBtn.addEventListener('click', function (e) {
+        e.preventDefault();
+        reviewBox.style.display = 'none';
+        newReview.focus();
+        openReviewBtn.style.display = 'block';
+        closeReviewBtn.style.display = 'none';
+    });
+
+    $('.starrr').on('starrr:change', function (e, value) {
+        ratingsField.value = value;
+    });
+});
